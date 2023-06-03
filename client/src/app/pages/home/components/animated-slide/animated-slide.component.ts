1fce26aede65dc8570c7e866d79167abe3505b24
AxelColliaux <axel.colliaux@hotmail.fr>
2023-06-03 22:42:55 +0200

    Front home ok

---
+++ client/src/app/pages/home/components/animated-slide/animated-slide.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-slide',
+  templateUrl: './animated-slide.component.html',
+  styleUrls: ['./animated-slide.component.scss']
+})
+export class AnimatedSlideComponent {
+
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        168: "168px",
+        316: "316px",
+        464: "464px",
+        612: "612px",
+      },
+      top: {
+        168: "168px",
+        316: "316px",
+        464: "464px",
+        612: "612px",
+      },
+      colors: {
+        "green-slide": "#219725",
+        "purple-slide": "#69256E",
+        "red-slide": "#EB3D49",
+        "orange-slide": "#CB7D28",
+        "blue-slide": "#286A9A",
+      },
+    },
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
   },
   plugins: [],
 };
