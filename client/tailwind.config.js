51c335c7bb0e363ca4821c4e19e0d163054cc728
AxelColliaux <axel.colliaux@hotmail.fr>
2023-06-04 16:03:27 +0200

    Slide component ok, parametrage ok

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,18 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
-      spacing: {
-        168: "168px",
-        316: "316px",
-        464: "464px",
-        612: "612px",
-      },
-      top: {
-        168: "168px",
-        316: "316px",
-        464: "464px",
-        612: "612px",
-      },
       colors: {
         "green-slide": "#219725",
         "purple-slide": "#69256E",
