b59bf92fb098c7887edfeb3298af68c1a791b423
m.birepinte <mickael.birepinte@gmail.com>
2024-03-14 15:34:37 +0100

    change websocket env

--- client/src/app/services/room-websocket.service.ts
+++ client/src/app/services/room-websocket.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Room } from '../models/room.model';
 import { Router } from '@angular/router';
+import {environment} from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +20,8 @@ export class RoomWebsocketService {
 
   connectWebSocket(): Promise<void> {
     return new Promise((resolve, reject) => {
-      this.webSocket = new WebSocket('ws://localhost:8080/websocket/room');
-  
+      this.webSocket = new WebSocket('ws://'+ environment.URL+'/websocket/room');
+
       this.webSocket.onopen = () => {
         console.log("Connexion websocket réussi");
         resolve();
@@ -28,19 +30,19 @@ export class RoomWebsocketService {
       this.webSocket.onclose = () => {
         this.webSocket.close();
       }
-  
+
       this.webSocket.onerror = (error) => {
         console.error("WebSocket error:", error);
         reject(error);
       };
-  
+
       this.webSocket.onmessage = (event) => {
         console.log(event);
         // console.log(this.webSocket.readyState);
 
         if (event.data !== "X") {
           const data = JSON.parse(event.data);
-    
+
           switch (data.type) {
             case 'FETCHING_ROOM_LIST':
               this.roomsSubject.next(data.rooms);
@@ -59,10 +61,10 @@ export class RoomWebsocketService {
         }
       };
     });
-  }  
+  }
 
   getRoomInfos(){
-    
+
     this.connectWebSocket().then((): void => {
       const message = { messageType: 'GET_ROOM_INFOS'};
       this.webSocket.send(JSON.stringify(message));
